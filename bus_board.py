c61ba74e54f34623513cf32bada570ef6144b1e8
wiesje <wiesje.heerik@softwire.com>
2020-06-11 14:58:20 +0100

    make postcode api request

--- bus_board.py
+++ bus_board.py
@@ -1,10 +1,12 @@
 from flask import Flask
-
+import requests
 app = Flask(__name__)
 
 
 @app.route('/<string:postcode>')
 def hello_world(postcode):
-    return postcode
+    response = requests.get(f"http://api.postcodes.io/postcodes/{postcode}")
+    print(response)
+    return response.json()
 
 if __name__ == "__main__": app.run()
