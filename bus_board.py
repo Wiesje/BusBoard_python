ac55f5ec35cc23966607dee825e9509ab2537c08
wiesje <wiesje.heerik@softwire.com>
2020-06-11 15:23:53 +0100

    Get list of stops

--- bus_board.py
+++ bus_board.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 @app.route('/<string:postcode>')
 def hello_world(postcode):
     response = requests.get(f"http://api.postcodes.io/postcodes/{postcode}")
-    print(response)
-    return response.json()
-
+    latitude = response.json()["result"]["latitude"]
+    longitude = response.json()["result"]["longitude"]
+    stops = requests.get(f"https://api.tfl.gov.uk/StopPoint?stopTypes=NaptanPublicBusCoachTram&lat={latitude}&lon={longitude}")
+    return stops.json()
 if __name__ == "__main__": app.run()
